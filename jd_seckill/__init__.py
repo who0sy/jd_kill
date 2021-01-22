67e779dc950fc988e781961f6458edad42b91e97
yangyuxing <yangyuxing@socmap.net>
2021-01-22 13:04:05 +0800

    clean

--- jd_seckill/__init__.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-Created on 2020/12/31 4:58 下午 
-@author: huangyiwei
-"""
--- jd_seckill/exception.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding=utf8 -*-
-
-
-class SKException(Exception):
-
-    def __init__(self, message):
-        super().__init__(message)
